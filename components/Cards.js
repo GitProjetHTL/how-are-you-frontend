c33a5cfcd6b6f66d12f664cc1e268f4aa02a9914
Félix msy <felixmansy@hotmail.com>
2023-04-12 17:41:21 +0200

    add cards & discover/cardsscreen css style

--- components/Cards.js
+++ components/Cards.js
@@ -1,28 +1,116 @@
-// Cards component 
+// Cards component
 import {
-    TouchableOpacity,
-    StyleSheet,
-    Text,
-    View,
-    Image,
-    TextInput,
-    SafeAreaView,
-  } from "react-native";
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 export default function Cards(props) {
-
-    return (
+  return (
     <>
-        <View style={styles.container}>
-            <Text>hey</Text>
-        </View>
-      </>
-    );
-  }
+      <View style={styles.cards}>
+        <Text style={styles.titleCard}>Anxieté</Text>
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor:"red",
-    },
-})
+        <Text style={styles.contentCard}>
+          {" "}
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum, et
+          animi repudiandae magni neque voluptatem. ... Lorem ipsum dolor sit,
+          amet consectetur adipisicing elit. Laboriosam sequi sed tempore, porro
+          neque qui nulla modi obcaecati vero ipsum doloremque, aliquam
+          explicabo necessitatibus, expedita harum ipsa. Sed, eum nesciunt alias
+          error dolorem exercitationem vitae enim sapiente, praesentium, beatae
+          ipsam numquam dignissimos accusamus ratione sunt laboriosam at
+          adipisci! Consequuntur voluptatum, recusandae quod aspernatur, totam,
+          libero dolorem nostrum nam quisquam nulla nihil. Consectetur quod
+          reprehenderit rem quisquam non quis saepe ad repellat, architecto eius
+          cumque illum totam consequuntur. Aperiam consectetur accusantium,
+          voluptas quod libero animi? Neque tempora libero eaque fuga quod
+          molestiae adipisci obcaecati reiciendis, dolore, natus animi cum. Quia
+          sit dolores eius minima tempora sunt repellendus necessitatibus
+          nostrum, rerum in. Sed ea totam, itaque ipsum earum eligendi tenetur
+          quam! Laboriosam et ab doloremque obcaecati, autem enim facere ea quas
+          reiciendis inventore explicabo exercitationem maiores alias saepe
+          eveniet odio velit nostrum qui ullam molestiae iure aliquam vitae
+          quibusdam architecto. Iste, corrupti!
+        </Text>
+        <View style={styles.btnContainer}>
+          <TouchableOpacity style={styles.moreButton}>
+            <Text style={styles.moreText}>Voir plus</Text>
+          </TouchableOpacity>
+          <View style={styles.heartContainer}>
+            <FontAwesome name="heart" size={20} style={styles.heart} />
+          </View>
+        </View>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  cards: {
+    backgroundColor: "white",
+    borderRadius: 25,
+    marginVertical: 15,
+    padding: 10,
+    width: "90%",
+    height: 200,
+  },
+  titleCard: {
+    fontFamily: "Solway-Bold",
+    fontSize: 24,
+  },
+  contentCard: {
+    // borderWidth: 1,
+    paddingTop: 2,
+    width: "100%",
+    height: 95,
+    fontSize: 16,
+  },
+  btnContainer: {
+    // borderWidth: 1,
+    height: 55,
+    alignItems: "center",
+
+    flexDirection: "row",
+  },
+  moreButton: {
+    // backgroundColor: "#5B3EAE",
+    borderColor: "#5B3EAE",
+    borderWidth: 1,
+    borderRadius: 25,
+    height: 40,
+    width: "50%",
+    marginBottom: 5,
+
+    // paddingTop: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  moreText: {
+    color: "#5B3EAE",
+    fontSize: 16,
+    fontWeight: 500,
+    textAlign: "center",
+  },
+  heartContainer: {
+    // borderWidth: 1,
+    backgroundColor: "#C3B6F4",
+    marginLeft: 20,
+    height: 40,
+    width: 40,
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 5,
+  },
+  heart: {
+    // flex: 1,
+    color: "white",
+  },
+});
--- screens/DiscoverScreen.js
+++ screens/DiscoverScreen.js
@@ -8,108 +8,118 @@ import {
   SafeAreaView,
 } from "react-native";
 
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import {useState , useEffect} from 'react'; 
-import Cards from "../components/Cards"
-import { useSelector } from 'react-redux';
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { useState, useEffect } from "react";
+import Cards from "../components/Cards";
+import { useSelector } from "react-redux";
 
 export default function DiscoverScreen() {
+  const [search, setSearch] = useState("");
 
-  const [search,setSearch] = useState("");
+  // //récupère le token
+  // // const user = useSelector((state) => state.user.value)
 
-  //récupère le token
-  // const user = useSelector((state) => state.user.value)
+  // const user = "LJduScVJOSgMuGBAxujt9Jk11dttUVz2";
 
-  const user="LJduScVJOSgMuGBAxujt9Jk11dttUVz2"
+  // const cardFilltered = "";
 
-  const cardFilltered=""; 
-
-  useEffect(() => {
-    fetch(`https://howareyouapp-backend.vercel.app/cards/all/${user}`)
-      .then(response => response.json())
-      .then(allCards => {
-        // console.log(allCards)
-      cardFilltered =  allCards.data.map((oneCard,i) => {
-          <Cards key={i} name={oneCard.name} content={oneCard.content} source={oneCard.source} />
-        })
-      });
-  }, []);
-  console.log(cardFilltered)
-
-
-
+  // useEffect(() => {
+  //   fetch(`https://howareyouapp-backend.vercel.app/cards/all/${user}`)
+  //     .then((response) => response.json())
+  //     .then((allCards) => {
+  //       // console.log(allCards)
+  //       cardFilltered = allCards.data.map((oneCard, i) => {
+  //         <Cards
+  //           key={i}
+  //           name={oneCard.name}
+  //           content={oneCard.content}
+  //           source={oneCard.source}
+  //         />;
+  //       });
+  //     });
+  // }, []);
+  // console.log(cardFilltered);
 
   return (
-    <>
     <SafeAreaView style={styles.container}>
       <View style={styles.containerTop}>
         <View>
-          <TextInput 
-              style={styles.input} 
-              placeholder="Recherches de cards" 
-              onChange={(e) => setSearch(e.target.value)} value={search}>
-          </TextInput>
-        </View>  
-        <View style={styles.likes}  >
+          <TextInput
+            style={styles.input}
+            placeholder="Recherches de cards"
+            onChange={(e) => setSearch(e.target.valueOf)}
+            value={search}
+          ></TextInput>
+        </View>
+        <View style={styles.likes}>
           <TouchableOpacity>
-            <FontAwesome name='heart' size={30}  />
+            <FontAwesome name="heart" size={30} style={styles.heart} />
           </TouchableOpacity>
         </View>
       </View>
       <View style={styles.title}>
-        <Text style={styles.text}>à la une</Text>
+        {/* <Text style={styles.text}>à la une</Text> */}
         <Text style={styles.sujet}>Sujet Aleatoire</Text>
       </View>
-      <View style={styles.cards}>
-        {cardFilltered}
+      <View style={styles.cardsContainer}>
+        <Cards
+          name="Nom de la carte"
+          content="Contenu de la carte"
+          source="Source de la carte"
+        />
       </View>
     </SafeAreaView>
-    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:"white",
+    backgroundColor: "white",
   },
 
   containerTop: {
-    marginTop:50,
-    flexDirection:'row',
+    marginTop: 30,
+    flexDirection: "row",
     justifyContent: "space-evenly",
-    backgroundColor:"white",
+    backgroundColor: "white",
   },
-  
-  
+
   input: {
     width: 270,
     height: 50,
     borderColor: "#5B3EAE",
     borderWidth: 1,
-    borderRadius: 5,
-    paddingLeft:10
+    borderRadius: 25,
+    paddingLeft: 10,
   },
-  likes:{
-    height:50,
-    justifyContent:"center",
+  likes: {
+    height: 50,
+    justifyContent: "center",
+    color: "#5B3EAE",
   },
-  title:{
-    justifyContent:"center",
-    alignItems:"center",
-    marginTop:50,
-    marginBottom:50,
+  heart: {
+    color: "#5B3EAE",
+  },
+  title: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 10,
   },
   sujet: {
-    fontSize:25,
-    fontFamily: "Solway-Bold",  
+    fontSize: 25,
+    fontFamily: "Solway-Bold",
   },
-  text:{
-    fontSize:16,
-    //fontFamily: "dm-sans-regular", 
+  text: {
+    fontSize: 16,
+    //fontFamily: "dm-sans-regular",
   },
-  cards:{
-    alignItems:"center",
-    justifyContent:"center",
-  }
+  cardsContainer: {
+    alignItems: "center",
+    // borderWidth: 1,
+    witdh: "100%",
+    height: "80%",
+    backgroundColor: "#E9EBFC",
+  },
 });
