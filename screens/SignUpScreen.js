13574c382248615bcaafa2628833d0258d857813
Nina <ninadoulache@gmail.com>
2023-04-12 14:04:31 +0200

    sign up screen

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Button,
   StyleSheet,
@@ -7,11 +8,57 @@ import {
   Image,
   SafeAreaView,
   TextInput,
+  KeyboardAvoidingView
 } from "react-native";
+import { useState, useRef } from 'react'; 
+import DatePicker from 'react-native-datepicker';
+import { Animated } from "react-native";
+
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/; 
 
 export default function SignUpScreen({ navigation }) {
+
+  const [username, setUsername ] = useState(''); 
+  const [usernameError, setUsernameError] = useState(false);
+  const [email, setEmail ] = useState('');
+  const [emailError, setEmailError] = useState(false);
+  // const [birthday, setBirthday] = useState('');
+  const [dateError, setDateError] = useState(false); 
+  const [password, setPassword ] = useState('');
+  const [passwordError, setPasswordError] = useState(false); 
+  const [date, setDate] = useState(new Date());
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  const handleSubmit = () => {
+    if (EMAIL_REGEX.test(email) 
+    && PASSWORD_REGEX.test(password)
+    && username.length > 0 && date.length > 0) {
+      
+      setEmail(email); // à modifier avec reducer
+      setPassword(password); // à modifier avec reducer
+      navigation.navigate('TabNavigator', { screen: 'Survey' });
+    } else {
+      setEmailError(true);
+      setPasswordError(true);
+      setDateError(true);
+      setUsernameError(true); 
+    }
+  };
+
+  const fadeIn = () => {
+    // Will change fadeAnim value to 1 in 5 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 5000,
+      useNativeDriver: true,
+    }).start();
+    setDate(date)
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
+
+    <View style={styles.container}>
       <View style={styles.imageContainer}>
         <Image
           source={require("../assets/register2.png")}
@@ -19,7 +66,7 @@ export default function SignUpScreen({ navigation }) {
           resizeMode="cover"
         />
       </View>
-      <View style={styles.signinContainer}>
+      <View style={styles.signupContainer}>
         <Text style={styles.title1}>Hello,how are you ?</Text>
         <Text style={styles.title2}>
           Bienvenue sur How are You ?, l'application qui t'aide à comprendre tes
@@ -28,33 +75,85 @@ export default function SignUpScreen({ navigation }) {
 
         <View style={styles.inputContainer}>
           <Text style={styles.label}>Comment tu t'appelles ?</Text>
-          <TextInput style={styles.input} placeholder="Entrez votre surnom" />
+          <TextInput 
+          style={styles.input} 
+          placeholder="Entrez votre surnom" 
+          onChange={(e) => setUsername(e.target.value)} value={username}/>
+          {usernameError && <Text style={styles.error}>Entrez un username</Text>}
         </View>
-        <View style={styles.inputContainer}>
+        {/* <View style={styles.inputContainer}>
           <Text style={styles.label}>Ta date de naissance</Text>
-          <TextInput style={styles.input} placeholder="DD/MM/YYYY" />
-        </View>
+          <TextInput 
+          style={styles.input} 
+          placeholder="DD/MM/YYYY" 
+          onChange={(e) => setBirthday(e.target.value)} value={birthday}/>
+          {birthdayError && <Text style={styles.error}>Entrez un birthday</Text>}
+        </View> */}
+        <View style={styles.inputContainer}>
+        <Text style={styles.label}>Ta date de naissance</Text>
+        <DatePicker
+          style={styles.datePickerStyle}
+          date={date}
+          mode="date"
+          placeholder="select date"
+          format="DD/MM/YYYY"
+          minDate="01-01-1900"
+          maxDate="01-01-2000"
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          customStyles={{
+            dateIcon: {
+              position: 'absolute',
+              right: -5,
+              top: 6,
+              marginLeft: 0,
+            },
+            dateInput: {
+              alignItems: "flex-start",
+              borderWidth: 0,
+              justifyContent: 'center'
+            },
+            placeholderText: {
+              fontSize: 17,
+              color: "gray", 
+            },
+            dateText: {
+              fontSize: 17,
+              color: 'gray'
+            }
+          }}
+          onDateChange={() => {
+            fadeIn();
+          }}/>
+          {dateError && <Text style={styles.error}>Entrez un birthday</Text>}
+          </View>
         <View style={styles.inputContainer}>
           <Text style={styles.label}>Mot de passe</Text>
           <TextInput
             style={styles.input}
             placeholder="********"
             secureTextEntry
-          />
+            onChange={(e) => setPassword(e.target.value)} value={password}/>
+             {passwordError && <Text style={styles.error}>Invalid password</Text>}
         </View>
+       
         <View style={styles.inputContainer}>
           <Text style={styles.label}>Adresse e-mail</Text>
-          <TextInput style={styles.input} placeholder="howyoufeel@gmail.com" />
-        </View>
+          <TextInput 
+          style={styles.input} 
+          placeholder="howyoufeel@gmail.com" 
+          onChange={(e) => setEmail(e.target.value)} value={email}/>
+          {emailError && <Text style={styles.error}>Invalid email address</Text>}        
+          </View>
 
         <TouchableOpacity
           style={styles.NextButton}
-          onPress={() => navigation.navigate("Survey")}
+          onPress={() => handleSubmit()}
         >
           <Text style={styles.NextText}>Suivant </Text>
         </TouchableOpacity>
       </View>
-    </SafeAreaView>
+    </View>
   );
 }
 const styles = StyleSheet.create({
@@ -63,6 +162,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "white",
+    borderColor: 'red', 
+    borderWidth: 2,
   },
   imageContainer: {
     marginTop: 50,
@@ -74,11 +175,13 @@ const styles = StyleSheet.create({
   image: {
     height: "100%",
   },
-  signinContainer: {
+  signupContainer: {
     height: "75%",
     width: "100%",
     alignItems: "center",
     borderWidth: 0,
+    // borderColor: 'red', 
+    // borderWidth: 2,
   },
   title1: {
     textAlign: "center",
@@ -92,6 +195,7 @@ const styles = StyleSheet.create({
     fontSize: 14,
     marginBottom: 20,
     fontFamily: "Solway-Regular",
+    padding: 10,
   },
   inputContainer: {
     alignItems: "center",
@@ -132,5 +236,18 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 500,
     textAlign: "center",
+  },
+  error: {
+    marginTop: 10,
+    color: 'red',
+  },
+  datePickerStyle: {
+    width: "80%",
+    height: 50,
+    borderColor: "#5B3EAE",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingLeft: 10,
+    paddingRight: 10,
   },
 });
