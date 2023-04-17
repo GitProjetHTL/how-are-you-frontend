5a021b6c71ce46d6cc9b59aa0cd23a467f6906e3
Nina <ninadoulache@gmail.com>
2023-04-17 09:54:52 +0200

    adding calendar in home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { TouchableOpacity, SafeAreaView, StyleSheet, Text, View, Image, TextInput } from "react-native";
+import { TouchableOpacity, SafeAreaView, StyleSheet, Text, View, Image, TextInput, Dimensions, ScrollView} from "react-native";
 import EmotionBoard from "../components/EmotionBoard";
 import { useSelector } from "react-redux";
-import {useState} from 'react'; 
+import { useState, useMemo } from 'react'; 
 import {Calendar, CalendarList, Agenda, LocaleConfig, HorizontalCalendar} from 'react-native-calendars';
 
 LocaleConfig.locales['fr'] = {
@@ -45,19 +45,32 @@ LocaleConfig.defaultLocale = 'fr';
 export default function HomeScreen({ navigation }) {
   const username = useSelector((state) => state.user.value.username); // affiche le pseudo
   const [selected, setSelected] = useState('');
-  let current = new Date()
+  let current = new Date();
+  const marked = useMemo(() => ({
+    [selected]: {
+      customStyles: {
+        container: {
+          backgroundColor: 'green',
+          borderRadius: 0,
+        },
+        text: {
+          color: 'white',
+        }
+      }
+    }
+  }), [selected]);
 
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>How are you, {username} ?</Text>
       <Text>Comment te sens-tu aujourd'hui ?</Text>
       <EmotionBoard />
-      <CalendarList
+      <Calendar
       style={{
         // borderWidth: 1,
         // borderColor: 'gray',
         // height: '50%',
-        width: '100%', 
+        width: Dimensions.get('window').width, 
         marginTop: '5%'
       }}
       theme={{
@@ -66,17 +79,18 @@ export default function HomeScreen({ navigation }) {
         textSectionTitleColor: '#C3B6F4',
         selectedDayBackgroundColor: '#C3B6F4',
         selectedDayTextColor: '#ffffff',
-        todayTextColor: '#C3B6F4',
-        dayTextColor: '#ffffff',
+        todayTextColor: 'red',
+        dayTextColor: '#C3B6F4',
         textDisabledColor: '#C3B6F4',}}
-      current={current}
+      current={new Date()}
       onDayPress={day => {
       console.log('selected day', day);
       setSelected(day.dateString);
   }}
-  markedDates={{
-    [selected]: {selected: true, disableTouchEvent: true, selectedDotColor: '#C3B6F4'}
-  }}
+  markedDates={marked}
+  // // {
+  //   [selected]: {selected: true, disableTouchEvent: true, selectedDotColor: '#C3B6F4'}
+  // }
 />
     </SafeAreaView>
   );
